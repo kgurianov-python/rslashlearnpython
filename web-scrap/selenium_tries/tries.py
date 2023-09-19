7317adfd8966d7a83d9c02854e688e3db337f5fc
Kostianytn Gurianov <kguryanov@gmail.com>
2023-09-19 23:55:48 +0300

    reverted toa cleaner copy

--- web-scrap/selenium_tries/tries.py
+++ web-scrap/selenium_tries/tries.py
@@ -1,5 +1,8 @@
-
+"""
+A simple template for selenium-based scripts
+"""
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -9,17 +12,11 @@ options = Options()
 # options.add_argument('--headless=new')
 # options.add_argument('--disable-gpu')  # Last I checked this was necessary.
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver.get('https://inventwithpython.com')
-try:
-    elem = driver.find_element(By.CLASS_NAME, 'card-img-top')
-    print(f'found {elem.tag_name} element with that class name!')
-except:
-    print('Was not able to find an element with that name')
-
-
-def main():
-    pass
-
 
 if __name__ == '__main__':
-    main()
+    driver.get('https://inventwithpython.com')
+    try:
+        elem = driver.find_element(By.CLASS_NAME, 'card-img-top')
+        print(f'found {elem.tag_name} element with that class name!')
+    except NoSuchElementException:
+        print('Was not able to find an element with that name')
